e3205c4b4044587fc17abbcfe6076dc85d2bd5c1
Zoe Dumas <zoe.dumas1@yahoo.com>
2018-07-13 14:12:47 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
 # book.rb
 # Add you Book class here
+class Book
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :title
+
+  def initialize(title)
+    @title = title
+  end
+
+  def author(author)
+    @author = author
+    puts "Agatha Christie"
+end
+def author
+  @author
+end
+end
